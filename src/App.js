766022dbe6277abcd59c03c6d4ef9a70170a3b9e
oty-git <oty872020@gmail.com>
2023-07-12 16:26:50 +0300

    refactoring app.js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,53 @@
 import './App.css';
-import {Switch, Route} from "react-router-dom";
+import {Switch, Route} from 'react-router-dom';
 import routes from './routes';
-import {useDispatch} from "react-redux";
-import {getLanguages, getCurrencies, getServices, getAppLanguages} from "./store/actions/init";
-import React,{useEffect} from "react";
-import PrivateRoute from "./PrivateRoute";
-import {useTranslation} from "react-i18next";
-
-
+import {useDispatch} from 'react-redux';
+import {
+  getLanguages,
+  getCurrencies,
+  getServices,
+  getAppLanguages,
+} from './store/actions/init';
+import React, {useEffect} from 'react';
+import PrivateRoute from './PrivateRoute';
 
 const App = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const {i18n} = useTranslation();
+  useEffect(() => {
+    dispatch(getLanguages());
+    dispatch(getAppLanguages());
+    dispatch(getCurrencies());
+    dispatch(getServices());
+  }, [dispatch]);
 
-    useEffect(() => {
-        dispatch(getLanguages());
-        dispatch(getAppLanguages());
-        dispatch(getCurrencies());
-        dispatch(getServices());
-    }, []);
-    
-    console.log('routes', routes)
-    return (
-        <Switch>
-            {routes.map(route => {
-                let componentProps = (route.props !== undefined) ? route.props : {};
-                let RouteComponent = route.component;
-                route.private = true;
+  return (
+    <Switch>
+      {routes.map((route) => {
+        let componentProps = route.props !== undefined ? route.props : {};
+        let RouteComponent = route.component;
+        route.private = true;
 
-                return route.private ?
-                    <PrivateRoute key={route.path} path={route.path} exact={route.exact} component={RouteComponent}/>
-                    :  <Route key={route.path} path={route.path} exact={route.exact}
-                              render={(routeProps) => (
-                                  <RouteComponent {...routeProps} {...componentProps} />
-                              )}
-                    />
-            })}
-        </Switch>
-    );
-}
+        return route.private ? (
+          <PrivateRoute
+            key={route.path}
+            path={route.path}
+            exact={route.exact}
+            component={RouteComponent}
+          />
+        ) : (
+          <Route
+            key={route.path}
+            path={route.path}
+            exact={route.exact}
+            render={(routeProps) => (
+              <RouteComponent {...routeProps} {...componentProps} />
+            )}
+          />
+        );
+      })}
+    </Switch>
+  );
+};
 
 export default App;
